408c4f303edbbbeb5327f8027783200918a28a78
hafez-ziadi <hafez@luneandwild.co.uk>
2025-10-21 23:46:39 +0000

    force Tailwind via Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/postcss'
 export default defineConfig({
   plugins: [react()],
+  css:{ postcss:{ plugins:[tailwindcss(), autoprefixer()] } }
 })
